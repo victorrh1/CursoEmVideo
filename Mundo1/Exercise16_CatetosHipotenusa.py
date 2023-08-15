04784d571778c148073e22ce5482bcb618318cf9
J. Victor.py <55515144+victorrh1@users.noreply.github.com>
2023-08-15 15:47:41 -0300

    Delete Exercise16_CatetosHipotenusa.py

--- Mundo1/Exercise16_CatetosHipotenusa.py
+++
@@ -1,9 +0,0 @@
-'''
-Criar um programa que leia o comprimento do cateto oposto e do cateto adjacente de um
-triangulo. Calcular e mostrar o comprimento da hipotenusa.
-'''
-from math import hypot
-co = float(input('Comprimento do cateto oposto: '))
-ca = float(input('Comprimento do cateto adjacente: '))
-h = hypot(co, ca)
-print('A hipotenusa vai medir {:.2f}'.format(h))
