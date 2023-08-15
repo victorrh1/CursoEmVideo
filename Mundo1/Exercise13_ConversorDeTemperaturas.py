1c5e84f74bce6a0cfca0812a6a54253202129829
J. Victor.py <55515144+victorrh1@users.noreply.github.com>
2023-08-15 15:47:14 -0300

    Delete Exercise13_ConversorDeTemperaturas.py

--- Mundo1/Exercise13_ConversorDeTemperaturas.py
+++
@@ -1,7 +0,0 @@
-'''
-Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para
-graus Farenheit.
-'''
-celsius = float(input('Informe a temperatura em °C: '))
-farenheit = celsius * 9 / 5 + 32
-print('A temperatura de {}°C corresponde a {}°F'.format(celsius, farenheit))
