5d07a1bb13917d5278a4815e2b07144b558c229b
J. Victor.py <55515144+victorrh1@users.noreply.github.com>
2023-08-15 15:49:26 -0300

    Delete Exercise23_VerificandoAsPrimeirasLetras.py

--- Mundo1/Exercise23_VerificandoAsPrimeirasLetras.py
+++
@@ -1,6 +0,0 @@
-'''
-Criar um sistema que leia o nome de uma cidade e diga se ela começa ou não com o nome 'SANTO'
-'''
-city = str(input('Em que cidade você nasceu? ')).strip()
-city = city.upper()
-print(city[0:5] == 'SANTO')
