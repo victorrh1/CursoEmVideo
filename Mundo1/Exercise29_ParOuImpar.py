2753bce3e4949a7f0b93a65afbed489088e49e2f
J. Victor.py <55515144+victorrh1@users.noreply.github.com>
2023-08-15 15:50:02 -0300

    Delete Exercise29_ParOuImpar.py

--- Mundo1/Exercise29_ParOuImpar.py
+++
@@ -1,8 +0,0 @@
-'''
-Criar um programa que leia um numero inteiro e mostrar se ele é PAR ou IMPAR.
-'''
-num = int(input('Digite um numero: '))
-if num % 2 == 0:
-    print(f'O numero {num} é PAR')
-else:
-    print(f'O numero {num} é IMPAR')
