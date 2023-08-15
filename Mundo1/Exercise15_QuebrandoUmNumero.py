189eff5bc65dbbbb64edbbf37d029f061e77eb3f
J. Victor.py <55515144+victorrh1@users.noreply.github.com>
2023-08-15 15:47:33 -0300

    Delete Exercise15_QuebrandoUmNumero.py

--- Mundo1/Exercise15_QuebrandoUmNumero.py
+++
@@ -1,7 +0,0 @@
-'''
-Criar um programa que leia um numero Real qualquer e mostre a sua porção inteira.
-'''
-import math
-numero = float(input('Digite um valor: '))
-valor = math.floor(numero)
-print('O valor digitado foi {} e a sua porção inteira é {}'.format(numero, valor))
