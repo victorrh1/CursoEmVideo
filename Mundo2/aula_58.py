ab4af058fce914ffed21624358ee824d99b169cc
J. Victor.py <55515144+victorrh1@users.noreply.github.com>
2025-03-19 00:14:53 -0300

    Criando um Menu de Opções

--- Mundo2/aula_58.py
+++ Mundo2/aula_58.py
@@ -12,16 +12,16 @@ while opção != 5:
     opção = int(input('Qual é a sua opção? '))
     if opção == 1:
         soma = n1 + n2
-        print('A soma entre {} + {} é {}'.format(n1, n2, soma))
+        print(f'A soma entre {n1} + {n2} é {soma}')
     elif opção == 2:
         produto = n1 * n2
-        print('O resultado de {} x {} é {}'.format(n1, n2, produto))
+        print(f'O resultado de {n1} x {n2} é {produto}')
     elif opção == 3:
         if n1 > n2:
             maior = n1
         else:
             maior = n2
-        print('Entre {} e {} o maior valor é {}'.format(n1, n2, maior))
+        print(f'Entre {n1} e {n2} o maior valor é {maior}')
     elif opção == 4:
         print('Informe os números novamente: ')
         n1 = int(input('Primeiro valor: '))
